e05e389b0f2058c3bfdb16c09d21b81aacadf34b
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2025-08-26 16:43:01 +0900

    Title: 구간 잘 나누기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250826/구간 잘 나누기/divide-sections-well.py
+++ 250826/구간 잘 나누기/divide-sections-well.py
@@ -1,30 +1,28 @@
 n, m = map(int, input().split())
 a = list(map(int, input().split()))
 
-# Please write your code here.
-min_val, max_val = max(a), sum(a)
-ans = max_val
+left, right = max(a), sum(a)
+ans = right
 
-for i in range(min_val, max_val + 1):
+while left <= right:
+    mid = (left + right) // 2
     range_sum = 0
-    # range_sum_list = []
     cnt = 0
     range_sum_max = 0
     for j in a:
-        if range_sum + j > i:
-            # range_sum_list.append(range_sum)
+        if range_sum + j > mid:
             cnt += 1
             range_sum_max = max(range_sum_max, range_sum)
             range_sum = j
         else:
             range_sum += j
     
-    # range_sum_list.append(range_sum)
     cnt += 1
     range_sum_max = max(range_sum_max, range_sum)
-    # print(i, range_sum_list, cnt, range_sum_max)
     if cnt <= m:
         ans = min(ans, range_sum_max)
-        # print(i, range_sum_list, cnt, range_sum_max)
+        right = mid - 1
+    else:
+        left = mid + 1
 
 print(ans)
