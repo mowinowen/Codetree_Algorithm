e9cdc83271e16e88b34488f0920b08b0342ab435
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2025-08-27 18:05:05 +0900

    Title: 최대 H 점수 2, Time: 61ms, Memory: 16MB, Status: Passed - Codetree

--- 250827/최대 H 점수 2/maximum-h-score-2.py
+++ 250827/최대 H 점수 2/maximum-h-score-2.py
@@ -1,26 +1,21 @@
-from bisect import bisect_left, bisect_right
+from bisect import bisect_left
+from collections import Counter
 
 N, L = map(int, input().split())
 a = list(map(int, input().split()))
 
 # Please write your code here.
 a.sort()
-ans = 0
-for i in range(1, 101):
-    # print(i, N - bisect_left(a, i))
-    if N - bisect_left(a, i) < i:
-        cnt = 0
-        for j in range(bisect_left(a, i) - 1, -1, -1):
-            if a[j] == i - 1:
-                cnt += 1
-            else:
-                break
-        # print(i, cnt, cnt + N - bisect_left(a, i))
-        if cnt <= L and cnt + N - bisect_left(a, i) >= i:
-            ans = max(ans, i)
-    else:
-        ans = max(ans, i)
-print(ans)
 
-    
-        
+ans = 0
+for i in range(N, 0, -1):
+    bl = bisect_left(a, i)
+    if N - bl < i:
+        cnt = Counter(a)[i - 1]
+        if cnt <= L and cnt + N - bl >= i:
+            ans = i
+            break
+    else:
+        ans = i
+        break
+print(ans)
