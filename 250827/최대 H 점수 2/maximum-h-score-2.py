e8414cbac9080537ed2732c8cbba8ea24e025e8a
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2025-08-27 18:06:28 +0900

    Title: 최대 H 점수 2, Time: 57ms, Memory: 16MB, Status: Passed - Codetree

--- 250827/최대 H 점수 2/maximum-h-score-2.py
+++ 250827/최대 H 점수 2/maximum-h-score-2.py
@@ -6,12 +6,13 @@ a = list(map(int, input().split()))
 
 # Please write your code here.
 a.sort()
+count_dict = Counter(a)
 
 ans = 0
 for i in range(N, 0, -1):
     bl = bisect_left(a, i)
     if N - bl < i:
-        cnt = Counter(a)[i - 1]
+        cnt = count_dict[i - 1]
         if cnt <= L and cnt + N - bl >= i:
             ans = i
             break
