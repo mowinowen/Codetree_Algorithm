d37e1794272f28fc4b485b8883f74440a333b012
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2025-08-27 16:54:07 +0900

    Title: 최대 H 점수 2, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250827/최대 H 점수 2/maximum-h-score-2.py
@@ -0,0 +1,26 @@
+from bisect import bisect_left, bisect_right
+
+N, L = map(int, input().split())
+a = list(map(int, input().split()))
+
+# Please write your code here.
+a.sort()
+ans = 0
+for i in range(1, 10):
+    # print(i, N - bisect_left(a, i))
+    if N - bisect_left(a, i) < i:
+        cnt = 0
+        for j in range(bisect_left(a, i) - 1, -1, -1):
+            if a[j] == i - 1:
+                cnt += 1
+            else:
+                break
+        # print(i, cnt, cnt + N - bisect_left(a, i))
+        if cnt <= L and cnt + N - bisect_left(a, i) >= i:
+            ans = max(ans, i)
+    else:
+        ans = max(ans, i)
+print(ans)
+
+    
+        
