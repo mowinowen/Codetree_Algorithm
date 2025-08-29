3d23472ff4e69507b8b1e41b84e7b010687989a6
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2025-08-29 10:27:01 +0900

    Title: 전부 겹치는 선분, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250829/전부 겹치는 선분/overlapping-line-segments.py
@@ -0,0 +1,22 @@
+n = int(input())
+segments = []
+
+for _ in range(n):
+    x1, x2 = map(int, input().split())
+    segments.append((x1, 1))
+    segments.append((x2 + 1, -1))
+
+segments.sort()
+
+cnt = 0
+isbool = False
+for x, line in segments:
+    cnt += line
+    if cnt == n:
+        isbool = True
+        break
+
+if isbool:
+    print('Yes')
+else:
+    print('No')
