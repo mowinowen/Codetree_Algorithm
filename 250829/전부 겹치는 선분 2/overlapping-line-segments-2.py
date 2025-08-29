e5d0efd5e49c5ef34a608495a93e77b7b32e67e2
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2025-08-29 11:47:38 +0900

    Title: 전부 겹치는 선분 2, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250829/전부 겹치는 선분 2/overlapping-line-segments-2.py
+++ 250829/전부 겹치는 선분 2/overlapping-line-segments-2.py
@@ -1,21 +1,25 @@
 n = int(input())
+
+max_left = float('-inf')
+min_right = float('inf')
 segments = []
 
 for _ in range(n):
     x1, x2 = map(int, input().split())
-    segments.append((x1, 1))
-    segments.append((x2 + 1, -1))
+    max_left = max(x1, max_left)
+    min_right = min(x2, min_right)
+    segments.append((x1, x2))
 
-segments.sort()
+second_max_left = float('-inf')
+second_min_right = float('inf')
+for seg_x1, seg_x2 in segments:
+    if seg_x1 != max_left:
+        second_max_left = max(second_max_left, seg_x1)
+    if seg_x2 != min_right:
+        second_min_right = min(second_min_right, seg_x2)
 
-cnt = 0
-isbool = False
-for x, line in segments:
-    cnt += line
-    if cnt == n - 1:
-        isbool = True
-        print('Yes')
-        break
+if second_max_left <= min_right or max_left <= second_min_right:
+    print('Yes')
 
-if not isbool:
+else:
     print('No')
