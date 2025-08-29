f33ca5dc66357bef7ba1155d90ea4a54a9e37ec2
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2025-08-29 10:19:44 +0900

    Title: 구역 청소, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250829/구역 청소/district-cleaning.py
+++ 250829/구역 청소/district-cleaning.py
@@ -2,4 +2,8 @@ a, b = map(int, input().split())
 c, d = map(int, input().split())
 
 # Please write your code here.
-print(max(a, b, c, d) - min(a, b, c, d))
+if b < c or d < a:
+    print(b - a + d - c)
+
+else:
+    print(max(a, b, c, d) - min(a, b, c, d))
