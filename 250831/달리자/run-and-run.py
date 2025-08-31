61d02836632b03d80f6cf72e3189d135e86fa79d
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2025-08-31 13:25:32 +0900

    Title: 달리자, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250831/달리자/run-and-run.py
+++ 250831/달리자/run-and-run.py
@@ -4,36 +4,9 @@ B = list(map(int, input().split()))
 
 # Please write your code here.
 ans = 0
-for i in range(n - 1, 0, -1):
-    val = B[i] - A[i]
-    for j in range(i - 1, -1, -1):
-        if val - A[j] > 0:
-            A[i] += A[j]
-            ans += A[j] * (i - j)
-            val -= A[j]
-            A[j] = 0
-        else:
-            A[i] += val
-            ans += val * (i - j)
-            A[j] -= val
-            val -= val
-        
-        if val == 0:
-            break
+for i in range(n):
+    if A[i] > B[i]:
+        ans += A[i] - B[i]
+        A[i + 1] += A[i] - B[i]
 
 print(ans)
-        
-
-'''
-5 4 3 1
-5 3 0 5
-
-
-3 6 4 2 1
-3 6 4 0 3 -> 2
-3 6 2 0 5 -> 4
-3 6 0 2 5 -> 2
-3 5 0 3 5 -> 2
-3 1 4 3 5 -> 4
-2 2 4 3 5 -> 1
-'''
