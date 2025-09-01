fcb8c35e50f5f638b960139799f82abe82cb8eb9
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2025-09-01 17:03:56 +0900

    Title: X 달리기, Time: 55ms, Memory: 16MB, Status: Passed - Codetree

--- 250901/X 달리기/x-run.py
+++ 250901/X 달리기/x-run.py
@@ -4,7 +4,12 @@ X = int(input())
 
 # Please write your code here.
 max_v = math.sqrt(X)
-if int(max_v) < max_v < int(max_v) + 1:
-    print(2 * int(max_v))
-else:
+# print(max_v)
+
+if int(max_v) == max_v:
     print(2 * int(max_v) - 1)
+else:
+    if 1 <= X - int(max_v) ** 2 <= int(max_v):
+        print(2 * int(max_v))
+    else:
+        print(2 * int(max_v) + 1)
