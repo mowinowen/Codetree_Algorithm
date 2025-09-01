d2f193c3007efc0bdf2c4c8119a1bc2c76ea70a7
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2025-09-01 13:29:58 +0900

    Title: 코딩톡, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 250901/코딩톡/coding-talk.py
+++ 250901/코딩톡/coding-talk.py
@@ -3,16 +3,21 @@ people = set([chr(ord('A') + i) for i in range(n)])
 info = [tuple(input().split()) for _ in range(m)]
 
 # print(info[p - 1])
-for i in range(p - 1, m):
-    if info[i][0] in people:
-        people.remove(info[i][0])
 
-for i in range(p - 2, -1, -1):
-    if info[i][1] == info[p - 1][1]:
+if info[p - 1][1] == '0':
+    people = set()
+
+else:
+    for i in range(p - 1, m):
         if info[i][0] in people:
             people.remove(info[i][0])
-    else:
-        break
+
+    for i in range(p - 2, -1, -1):
+        if info[i][1] == info[p - 1][1]:
+            if info[i][0] in people:
+                people.remove(info[i][0])
+        else:
+            break
 
 for p in sorted(list(people)):
     print(p, end=' ')
