80748a39bba55ef6a2c2b0d40e6388458baf1824
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2025-09-02 16:08:13 +0900

    Title: 독서실의 거리두기 3, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250902/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
+++ 250902/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
@@ -1,21 +1,6 @@
 N = int(input())
 seats = list(input())
 
-# Please write your code here.
-# seated = [i for i, j in enumerate(seats) if j == '1']
-
-# print(seated)
-ans = float('-inf')
-for i in range(1, N - 1):
-    if seats[i] == '0':
-        seats[i] = '1'
-        seated = [i for i, j in enumerate(seats) if j == '1']
-        
-        curr = float('inf')
-        for j in range(1, len(seated)):
-            curr = min(seated[j] - seated[j - 1], curr)
-        seats[i] = '0'
-    
-        ans = max(curr, ans)
-
-print(ans)
+pos = [i for i, j in enumerate(seats) if j == '1']
+gap = [pos[i] - pos[i - 1] for i in range(1, len(pos))]
+print(min(max(gap) // 2, min(gap)))
