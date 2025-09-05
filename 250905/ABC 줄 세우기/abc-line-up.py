ace441d4cafbb3e8a027451c5967679918429bda
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2025-09-05 11:59:32 +0900

    Title: ABC 줄 세우기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250905/ABC 줄 세우기/abc-line-up.py
+++ 250905/ABC 줄 세우기/abc-line-up.py
@@ -2,6 +2,11 @@ n = int(input())
 arr = list(input().split())
 
 # Please write your code here.
-cnt = sum(i == ord(val) - ord('A') for i, val in enumerate(arr))
+cnt = 0
+for i in range(n):
+    for j in range(i + 1, n):
+        if ord(arr[i]) - ord('A') > ord(arr[j]) - ord('A'):
+            arr[i], arr[j] = arr[j], arr[i]
+            cnt += 1
 
-print(n - cnt - 1)
+print(cnt)
