0f50041177f5774cfb6d5a97931cf2eef68432a1
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2025-09-07 15:13:06 +0900

    Title: 순위 경쟁, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 250907/순위 경쟁/ranking-competition.py
+++ 250907/순위 경쟁/ranking-competition.py
@@ -1,51 +1,22 @@
 n = int(input())
-a, b, c = 0, 0, 0
-curr = 'ABC'
+count_dict = {'A': 0, 'B': 0, 'C': 0}
+curr = {'A', 'B', 'C'}
 ans = 0
 
 for _ in range(n):
     ci, si = input().split()
     si = int(si)
-    if ci == 'A':
-        a += si
-    elif ci == 'B':
-        b += si
-    else:
-        c += si
+    count_dict[ci] += si
+    curr_tmp = set()
+
+    max_val = max(count_dict.values())
+    for k in count_dict:
+        if count_dict[k] == max_val:
+            curr_tmp.add(k)
     
-    if a > b and a > c:
-        if curr != 'A':
-            ans += 1
-        curr = 'A'
+    if curr != curr_tmp:
+        ans += 1
     
-    elif b > a and b > c:
-        if curr != 'B':
-            ans += 1
-        curr = 'B'
-    
-    elif c > b and c > a:
-        if curr != 'C':
-            ans += 1
-        curr = 'C'
-    
-    elif a == b and a > c:
-        if curr != 'AB':
-            ans += 1
-        curr = 'AB'
-    
-    elif a == c and a > b:
-        if curr != 'AC':
-            ans += 1
-        curr = 'AC'
-    
-    elif b == c and b > a:
-        if curr != 'BC':
-            ans += 1
-        curr = 'BC'
-    
-    else:
-        if curr != 'ABC':
-            ans += 1
-        curr = 'ABC'
+    curr = curr_tmp
 
 print(ans)
