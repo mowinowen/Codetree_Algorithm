6827ba80d8e33092c567642879d1590b602f1439
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2025-09-11 14:14:16 +0900

    Title: 수열의 순서 바꾸기, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250911/수열의 순서 바꾸기/reorder-sequence.py
+++ 250911/수열의 순서 바꾸기/reorder-sequence.py
@@ -7,6 +7,8 @@ num = sequence[-1]
 
 for i in range(n - 1, -1, -1):
     if num < sequence[i]:
-        print(i + 1)
+        ans = i + 1
         break
-    num = sequence[i]    
+    num = sequence[i]
+
+print(ans)
