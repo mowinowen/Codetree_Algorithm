97d01dafff7404a9a938625ac081f12b476fbc20
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2025-09-15 12:44:22 +0900

    Title: 두 점으로 만드는 정사각형, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250915/두 점으로 만드는 정사각형/square-made-of-two-points.py
@@ -0,0 +1,5 @@
+x1, y1, x2, y2 = map(int, input().split())
+a1, b1, a2, b2 = map(int, input().split())
+
+# Please write your code here.
+print(max(max(x2, a2) - min(x1, a1), max(y2, b2) - min(y1, b1)) ** 2)
