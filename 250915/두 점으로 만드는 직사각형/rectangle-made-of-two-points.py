f3e9911b9831fabe95dcedca0c5f4b7c4928bddf
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2025-09-15 10:31:18 +0900

    Title: 두 점으로 만드는 직사각형, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250915/두 점으로 만드는 직사각형/rectangle-made-of-two-points.py
+++ 250915/두 점으로 만드는 직사각형/rectangle-made-of-two-points.py
@@ -2,7 +2,7 @@ x1, y1, x2, y2 = map(int, input().split())
 a1, b1, a2, b2 = map(int, input().split())
 
 # Please write your code here.
-min_x, max_x = min(x1, x2, a1, a2), max(x1, x2, a1, a2)
-min_y, max_y = min(y1, y2, b1, b2), max(y1, y2, b1, b2)
+min_x, max_x = min(x1, a1), max(x2, a2)
+min_y, max_y = min(y1, b1), max(y2, b2)
 
 print((max_x - min_x) * (max_y - min_y))
