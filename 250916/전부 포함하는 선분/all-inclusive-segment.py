f0c46398c96decce5a0f1708384c966925ad2f7e
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2025-09-16 13:32:05 +0900

    Title: 전부 포함하는 선분, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250916/전부 포함하는 선분/all-inclusive-segment.py
+++ 250916/전부 포함하는 선분/all-inclusive-segment.py
@@ -2,10 +2,9 @@ n = int(input())
 arr = [tuple(map(int, input().split())) for _ in range(n)]
 # Please write your code here.
 
-sort_start_arr = sorted(arr)
-min_x1, min_x2 = sort_start_arr[0][0], sort_start_arr[1][0]
+sort_start_arr = sorted(arr)[1:]
+ans = max(x for _, x in sort_start_arr) - sort_start_arr[0][0]
 
-sort_end_arr = sorted(arr, key = lambda x : x[1])
-max_x1, max_x2 = sort_end_arr[-1][1], sort_end_arr[-2][1]
-
-print(min(max_x2 - min_x1, max_x1 - min_x2))
+sort_end_arr = sorted(arr, key = lambda x : x[1])[:-1]
+ans = min(ans, sort_end_arr[-1][1] - min(x for x, _ in sort_end_arr))
+print(ans)
