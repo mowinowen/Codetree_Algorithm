5c5eea68482724559ec3b6f8b8d932d6098f7188
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2025-10-02 11:29:07 +0900

    Title: 기수 정렬 구현, Time: 117ms, Memory: 26MB, Status: Passed - Codetree

--- 251002/기수 정렬 구현/implement-radix-sort.py
+++ 251002/기수 정렬 구현/implement-radix-sort.py
@@ -5,11 +5,7 @@ arr = list(map(int, input().split()))
 for i in range(6):
     radix_arr = [[] for _ in range(10)]
     for num in arr:
-        idx = num % 10 ** (i + 1) // 10 ** i
-        radix_arr[idx].append(num)
-    arr = []
-    for r in radix_arr:
-        for num in r:
-            arr.append(num)
+        radix_arr[num % 10 ** (i + 1) // 10 ** i].append(num)
+    arr = [num for r in radix_arr for num in r]
 
 print(*arr)
