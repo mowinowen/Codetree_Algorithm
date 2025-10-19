b3d11f682cbe4bfcb235fb1d572fa0e6b1e9e6a7
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2025-10-19 21:31:38 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251019/괄호 문자열의 적합성 판단/parentheses-string.py
+++ 251019/괄호 문자열의 적합성 판단/parentheses-string.py
@@ -6,7 +6,9 @@ for i in str:
     if i == '(':
         stack.append(i)
     else:
-        if len(stack) != 0:
+        if len(stack) == 0:
+            stack.append(i)
+        else:
             stack.pop()
 
 if len(stack) == 0:
