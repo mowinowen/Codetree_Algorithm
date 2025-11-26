47141c27c91a29a27f1440e229944ccdeac14d0d
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2025-11-26 16:08:10 +0900

    Title: 트로미노, Time: 94ms, Memory: 19MB, Status: Passed - Codetree

--- 251126/트로미노/tromino.py
+++ 251126/트로미노/tromino.py
@@ -5,20 +5,11 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 ans = 0
 for i in range(n - 1):
     for j in range(m - 1):
-        min_num = float('inf')
-        sum_num = 0
-        for a in range(i, i + 2):
-            for b in range(j, j + 2):
-                min_num = min(min_num, grid[a][b])
-                sum_num += grid[a][b]
-        ans = max(sum_num - min_num, ans)
+        box = [grid[i][j], grid[i+1][j], grid[i][j+1], grid[i+1][j+1]]
+        ans = max(sum(box) - min(box), ans)
 
 for row in grid + list(zip(*grid)):
-    sum_num = sum(row[:3])
-    ans = max(sum_num, ans)
-    if len(row) > 3:
-        for i in range(3, len(row)):
-            sum_num = sum_num - row[i - 3] + row[i]
-            ans = max(sum_num, ans)
+    for i in range(len(row) - 2):
+        ans = max(ans, sum(row[i : i + 3]))
 
 print(ans)
