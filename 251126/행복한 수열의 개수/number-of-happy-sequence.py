ead1c4c89ab0bca491ca55e58bc0de2bcdf22ede
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2025-11-26 15:28:30 +0900

    Title: 행복한 수열의 개수, Time: 61ms, Memory: 18MB, Status: Passed - Codetree

--- 251126/행복한 수열의 개수/number-of-happy-sequence.py
+++ 251126/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,28 +1,8 @@
+from itertools import groupby
+
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
-def number_counting(arr):
-    cnt = 0
-    curr = 1
-    max_cnt = 1
-    for i in range(1, n):
-        if arr[i - 1] == arr[i]:
-            curr += 1
-            max_cnt = max(max_cnt, curr)
-        else:
-            curr = 1
 
-    if max_cnt >= m:
-        cnt += 1
-
-    return cnt
-
-
-ans = 0
-for row in grid:
-    ans += number_counting(row)
-
-for col in zip(*grid):
-    ans += number_counting(col)
-print(ans)
+print(sum(any(len(list(j)) >= m for i, j in groupby(row)) for row in grid + list(zip(*grid))))
