94540624d7c0a8bb4d72c34c4df93e1738703895
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2025-12-01 17:13:14 +0900

    Title: 금 채굴하기, Time: 72ms, Memory: 18MB, Status: Wrong Answer - Codetree

--- 251201/금 채굴하기/gold-mining.py
+++ 251201/금 채굴하기/gold-mining.py
@@ -2,43 +2,16 @@ n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
-def mining_shape_0(x, y):
-    return grid[x][y]
-
-def mining_shape_1(x, y):
-    d = [(0, 0), (0, 1), (0, -1), (1, 0), (-1, 0)]
-    
-    sum_val = 0
-    for dx, dy in d:
-        nx, ny = x + dx, y + dy
-        if 0 <= nx < n and 0 <= ny < n and grid[nx][ny]:
-            sum_val += 1
-    
-    if 5 <= sum_val * m:
-        return sum_val
-    
-    else:
-        return 0
-
-def mining_shape_2(x, y):
-    d = [(0, 0), (0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1), (0, 2), (0, -2), (2, 0), (-2, 0)]
-
-    sum_val = 0
-    for dx, dy in d:
-        nx, ny = x + dx, y + dy
-        if 0 <= nx < n and 0 <= ny < n and grid[nx][ny]:
-            sum_val += 1
-
-    if 13 <= sum_val * m:
-        return sum_val
-    
-    else:
-        return 0
-
-
 ans = 0
-for i in range(n):
-    for j in range(n):
-        ans = max(mining_shape_0(i, j), mining_shape_1(i, j), mining_shape_2(i, j), ans)
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            sum_val = 0
+            for x in range(n):
+                for y in range(n):
+                    if abs(i - x) + abs(j - y) <= k and grid[x][y]:
+                        sum_val += 1
+            if k ** 2 + (k + 1) ** 2 <= sum_val * m:
+                ans = max(ans, sum_val)
 
 print(ans)
