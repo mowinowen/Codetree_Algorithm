92c68ccd0b09d13b1e5686fb1e7bf3dcce8a0511
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2025-12-01 17:47:32 +0900

    Title: 금 채굴하기, Time: 56ms, Memory: 17MB, Status: Passed - Codetree

--- 251201/금 채굴하기/gold-mining.py
+++ 251201/금 채굴하기/gold-mining.py
@@ -2,16 +2,14 @@ n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
+gold_pos = [(i, j) for i in range(n) for j in range(n) if grid[i][j] == 1]
+
 ans = 0
-for k in range(n):
-    for i in range(n):
-        for j in range(n):
-            sum_val = 0
-            for x in range(n):
-                for y in range(n):
-                    if abs(i - x) + abs(j - y) <= k and grid[x][y]:
-                        sum_val += 1
-            if k ** 2 + (k + 1) ** 2 <= sum_val * m:
-                ans = max(ans, sum_val)
+for i in range(n):
+    for j in range(n):
+        dists = sorted([abs(x - i) + abs(y - j) for x, y in gold_pos])
+        for idx, k in enumerate(dists):
+            if k ** 2 + (k + 1) ** 2 <= (idx + 1) * m:
+                ans = max(ans, idx + 1)
 
 print(ans)
