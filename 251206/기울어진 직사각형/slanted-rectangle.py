0c6e75951e3df58fb7d361f83b74fbbe5d7cd55c
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2025-12-06 23:29:34 +0900

    Title: 기울어진 직사각형, Time: 114ms, Memory: 20MB, Status: Passed - Codetree

--- 251206/기울어진 직사각형/slanted-rectangle.py
+++ 251206/기울어진 직사각형/slanted-rectangle.py
@@ -8,40 +8,22 @@ def in_range(x, y):
 
 def sum_value(r, c, x, y):
     sum_val = 0
-    for _ in range(1, r + 1):
-        if in_range(x + d[0][0], y + d[0][1]):
-            x, y = x + d[0][0], y + d[0][1]
-            sum_val += grid[x][y]
-        else:
-            return 0
-    
-    for _ in range(1, c + 1):
-        if in_range(x + d[1][0], y + d[1][1]):
-            x, y = x + d[1][0], y + d[1][1]
-            sum_val += grid[x][y]
-        else:
-            return 0
+    d = [(-1, 1), (-1, -1), (1, -1), (1, 1)]
 
-    for _ in range(1, r + 1):
-        if in_range(x + d[2][0], y + d[2][1]):
-            x, y = x + d[2][0], y + d[2][1]
-            sum_val += grid[x][y]
-        else:
-            return 0
-    
-    for _ in range(1, c + 1):
-        if in_range(x + d[3][0], y + d[3][1]):
-            x, y = x + d[3][0], y + d[3][1]
-            sum_val += grid[x][y]
-        else:
-            return 0
-    
+    for i in range(4):
+        m = r if i % 2 == 0 else c
+        for _ in range(1, m + 1):
+            if in_range(x + d[i][0], y + d[i][1]):
+                x, y = x + d[i][0], y + d[i][1]
+                sum_val += grid[x][y]
+            else:
+                return 0
     return sum_val
 
 ans = 0
-d = [(-1, 1), (-1, -1), (1, -1), (1, 1)]
-for x in range(n):
-    for y in range(n):
+
+for x in range(2, n):
+    for y in range(1, n - 1):
         for r in range(1, n):
             for c in range(1, n):
                 ans = max(ans, sum_value(r, c, x, y))
