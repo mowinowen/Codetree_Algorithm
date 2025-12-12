07f06a9c5105057cd6dd148e40f0ce350378806a
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2025-12-12 15:03:48 +0900

    Title: 양수 직사각형의 최대 크기, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251212/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
+++ 251212/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.py
@@ -1,6 +1,5 @@
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
-ans = -1
 # Please write your code here.
 
 def check_positive_rectangle(x1, y1, x2, y2):
@@ -14,7 +13,7 @@ def check_positive_rectangle(x1, y1, x2, y2):
 
     return cnt
 
-ans = float('-inf')
+ans = -1
 for x1 in range(n):
     for y1 in range(m):
         for x2 in range(x1 + 1, n):
