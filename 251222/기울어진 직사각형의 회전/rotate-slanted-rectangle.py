57e48b4e0c7d9aef2fbdfd2499d3942a6bc85970
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2025-12-22 16:03:19 +0900

    Title: 기울어진 직사각형의 회전, Time: 68ms, Memory: 18MB, Status: Passed - Codetree

--- 251222/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
+++ 251222/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
@@ -1,44 +1,30 @@
+from collections import deque
+
 n = int(input())
 grid = [[0] * (n + 1)] + [[0] + list(map(int, input().split())) for _ in range(n)]
 r, c, m1, m2, m3, m4, dir = map(int, input().split())
 
 # Please write your code here.
 
-tmp = grid[r][c]
-cur_r, cur_c = r, c
+d = [(-1, 1), (-1, -1), (1, -1), (1, 1)]
+lengths = [m1, m2, m3, m4]
+positions, vals = [], deque()
+
+for (dx, dy), m in zip(d, lengths):
+    for _ in range(m):
+        r += dx
+        c += dy
+        positions.append((r, c))
+        vals.append(grid[r][c])
+
 if dir == 0:
-    for i in range(1, m4 + 1):
-        grid[cur_r - i + 1][cur_c - i + 1] = grid[cur_r - i][cur_c - i]
-    cur_r, cur_c = cur_r - m4, cur_c - m4
-    
-    for i in range(1, m3 + 1):
-        grid[cur_r - i + 1][cur_c + i - 1] = grid[cur_r - i][cur_c + i]
-    cur_r, cur_c = cur_r - m3, cur_c + m3
-
-    for i in range(1, m2 + 1):
-        grid[cur_r + i - 1][cur_c + i - 1] = grid[cur_r + i][cur_c + i]
-    cur_r, cur_c = cur_r + m2, cur_c + m2
-
-    for i in range(1, m1):
-        grid[cur_r + i - 1][cur_c - i + 1] = grid[cur_r + i][cur_c - i]
-    grid[r - 1][c + 1] = tmp
+    vals.rotate(1)
 
 else:
-    for i in range(1, m1 + 1):
-        grid[cur_r - i + 1][cur_c + i - 1] = grid[cur_r - i][cur_c + i]
-    cur_r, cur_c = cur_r - m1, cur_c + m1
-
-    for i in range(1, m2 + 1):
-        grid[cur_r - i + 1][cur_c - i + 1] = grid[cur_r - i][cur_c - i]
-    cur_r, cur_c = cur_r - m2, cur_c - m2
-
-    for i in range(1, m3 + 1):
-        grid[cur_r + i - 1][cur_c - i + 1] = grid[cur_r + i][cur_c - i]
-    cur_r, cur_c = cur_r + m3, cur_c - m3
-
-    for i in range(1, m4):
-        grid[cur_r + i - 1][cur_c + i - 1] = grid[cur_r + i][cur_c + i]
-    grid[r - 1][c - 1] = tmp
+    vals.rotate(-1)
     
+for (x, y), val in zip(positions, vals):
+    grid[x][y] = val
+
 for row in grid[1:]:
     print(*row[1:])
