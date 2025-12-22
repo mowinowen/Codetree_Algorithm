69f4a7e59c55ab1ef2d73368f612e64529265d6a
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2025-12-22 17:11:16 +0900

    Title: 2차원 바람, Time: 105ms, Memory: 20MB, Status: Passed - Codetree

--- 251222/2차원 바람/The-2D-wind-blows.py
+++ 251222/2차원 바람/The-2D-wind-blows.py
@@ -1,5 +1,6 @@
 import sys
 input = sys.stdin.readline
+from collections import deque
 
 n, m, q = map(int, input().split())
 
@@ -14,24 +15,23 @@ winds = [tuple(map(int, input().split())) for _ in range(q)]
 def in_range(x, y):
     return 0 < x < n + 1 and 0 < y < m + 1
 
-d = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
 for r1, c1, r2, c2 in winds:
-    tmp = a[r1][c1]
+    lengths = [c2 - c1, r2 - r1, c2 - c1, r2 - r1]
+    positions, vals = [], deque()
 
-    for i in range(r1 + 1, r2 + 1):
-        a[i - 1][c1] = a[i][c1]
+    for (dx, dy), l in zip(dirs, lengths):
+        for _ in range(l):
+            r1 += dx
+            c1 += dy
+            positions.append((r1, c1))
+            vals.append(a[r1][c1])
     
-    for i in range(c1 + 1, c2 + 1):
-        a[r2][i - 1] = a[r2][i]
-    
-    for i in range(r2 - 1, r1 - 1, -1):
-        a[i + 1][c2] = a[i][c2]
+    vals.rotate(1)
 
-    for i in range(c2 - 1, c1, -1):
-        a[r1][i + 1] = a[r1][i]
-    
-    a[r1][c1 + 1] = tmp
+    for (x, y), val in zip(positions, vals):
+        a[x][y] = val
 
     update_val = {}
     for i in range(1, n + 1):
@@ -40,7 +40,7 @@ for r1, c1, r2, c2 in winds:
                 sum_val = a[i][j]
                 cnt = 1
 
-                for dx, dy in d:
+                for dx, dy in dirs:
                     if in_range(i + dx, j + dy):
                         sum_val += a[i + dx][j + dy]
                         cnt += 1
