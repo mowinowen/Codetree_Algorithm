0f5cc466b150398c8b7e2234460760b92e0bb501
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2026-01-04 23:37:12 +0900

    Title: 2차원 폭발 게임, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260104/2차원 폭발 게임/The-2D-bomb-game.py
+++ 260104/2차원 폭발 게임/The-2D-bomb-game.py
@@ -1,6 +1,4 @@
 import sys
-from itertools import groupby, chain
-
 input = sys.stdin.readline
 
 n, m, k = map(int, input().split())
@@ -11,21 +9,45 @@ def bomb_grid(n, k):
     new_grid = []
     for col in zip(*numbers_2d):
         col = [c for c in col if c]
+        # print(col)
         while True:
             if not col:
                 new_grid.append([0] * n)
                 break
+
+            if len(col) == 1:
+                new_grid.append([0] * (n - 1) + col)
+                break
+            
             new_col = []
             isbool = False
-            for val, group in groupby(col):
-                g_list = list(group)
+            # for val, group in groupby(col):
+            #     g_list = list(group)
                 
-                if len(g_list) >= m:
-                    isbool = True
+            #     if len(g_list) >= m:
+            #         isbool = True
+            #     else:
+            #         new_col += g_list
+
+            curr = col[0]
+            cnt = 1
+            for num in col[1:]:
+                if curr == num:
+                    cnt += 1
                 else:
-                    new_col += g_list
+                    if cnt < m:
+                        new_col += [curr] * cnt
+                    else:
+                        isbool = True
+                    curr = num
+                    cnt = 1
             
+            if cnt < m:
+                new_col += [curr] * cnt
+            else:
+                isbool = True
             col = new_col
+            # print(col)
 
             if not isbool:
                 new_grid.append([0] * (n - len(col)) + col)
@@ -40,7 +62,8 @@ for num in range(k):
     numbers_2d = list(map(list, zip(*new_grid[::-1])))
     numbers_2d = bomb_grid(n, k)
 
-    grid_key = tuple(chain.from_iterable(numbers_2d))
+    # grid_key = tuple(chain.from_iterable(numbers_2d))
+    grid_key = tuple(val for row in numbers_2d for val in row)
 
     if grid_key in history:
         time = (k - num) % (num - history[grid_key])
