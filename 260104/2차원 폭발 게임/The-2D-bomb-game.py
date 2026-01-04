d1246aa82aa08a488ecf4386387c196aee966c3c
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2026-01-04 22:11:21 +0900

    Title: 2차원 폭발 게임, Time: 1374ms, Memory: 80MB, Status: Memory Limit Exceed - Codetree

--- 260104/2차원 폭발 게임/The-2D-bomb-game.py
+++ 260104/2차원 폭발 게임/The-2D-bomb-game.py
@@ -7,34 +7,14 @@ n, m, k = map(int, input().split())
 numbers_2d = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
-
-for _ in range(k):
-    new_grid = []
-    for col in zip(*numbers_2d):
-        while True:
-            new_col = []
-            isbool = False
-            for val, group in groupby(col):
-                g_list = list(group)
-                
-                if len(g_list) >= m:
-                    isbool = True
-                else:
-                    new_col += g_list
-            
-            col = new_col
-
-            if not isbool:
-                new_grid.append([0] * (n - len(col)) + col)
-                break
-
-    new_grid = list(map(list, zip(*new_grid)))
-    numbers_2d = list(map(list, zip(*new_grid[::-1])))
-
+def bomb_grid(n, k):
     new_grid = []
     for col in zip(*numbers_2d):
         col = [c for c in col if c]
         while True:
+            if not col:
+                new_grid.append([0] * n)
+                break
             new_col = []
             isbool = False
             for val, group in groupby(col):
@@ -51,6 +31,25 @@ for _ in range(k):
                 new_grid.append([0] * (n - len(col)) + col)
                 break
 
-    numbers_2d = list(map(list, zip(*new_grid)))
+    # numbers_2d = list(map(list, zip(*new_grid)))
+    return list(map(list, zip(*new_grid)))
 
+history = {}
+for num in range(k):
+    new_grid = bomb_grid(n, k)
+    numbers_2d = list(map(list, zip(*new_grid[::-1])))
+    numbers_2d = bomb_grid(n, k)
+
+    # history[tuple(map(tuple, numbers_2d))] = num
+    if tuple(map(tuple, numbers_2d)) in history:
+        time = (k - num) % (num - history[tuple(map(tuple, numbers_2d))])
+
+        for _ in range(time):
+            new_grid = bomb_grid(n, k)
+            numbers_2d = list(map(list, zip(*new_grid[::-1])))
+            numbers_2d = bomb_grid(n, k)
+        break
+    
+    history[tuple(map(tuple, numbers_2d))] = num
+    
 print(n ** 2 - sum(row.count(0) for row in numbers_2d))
