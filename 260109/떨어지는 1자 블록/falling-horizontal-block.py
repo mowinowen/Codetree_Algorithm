f46fd2ba0dce292c4f9c109c2dcda9bbabf528b4
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2026-01-09 11:48:06 +0900

    Title: 떨어지는 1자 블록, Time: 61ms, Memory: 16MB, Status: Passed - Codetree

--- 260109/떨어지는 1자 블록/falling-horizontal-block.py
+++ 260109/떨어지는 1자 블록/falling-horizontal-block.py
@@ -6,21 +6,13 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
 isbool = True
+r = n - 1
 for i in range(n):
-    for j in range(m):
-        if grid[i][k - 1 + j] == 1:
-            isbool = False
-            r = i - 1
-            break
-        
-    if not isbool:
+    if 1 in grid[i][k - 1 : k + m - 1]:
+        r = i - 1
         break
 
-if isbool:
-    r = n - 1
-
-for i in range(m):
-    grid[r][k - 1 + i] = 1
+grid[r][k - 1 : k + m - 1] = [1] * m
 
 for row in grid:
     print(*row)
