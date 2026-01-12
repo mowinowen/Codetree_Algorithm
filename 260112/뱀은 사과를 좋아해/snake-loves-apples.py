c1007ffe5a117549c495280c89af4aebaeddd581
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2026-01-12 16:50:51 +0900

    Title: 뱀은 사과를 좋아해, Time: 51ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260112/뱀은 사과를 좋아해/snake-loves-apples.py
@@ -0,0 +1,21 @@
+N, M, K = map(int, input().split())
+
+x, y = [], []
+for _ in range(M):
+    xi, yi = map(int, input().split())
+    x.append(xi)
+    y.append(yi)
+
+d, p = [], []
+for _ in range(K):
+    di, pi = input().split()
+    d.append(di)
+    p.append(int(pi))
+
+# Please write your code here.
+'''
+0 0 0 0 0
+0 0 0 0 0
+0 0 0 0 0
+
+'''
