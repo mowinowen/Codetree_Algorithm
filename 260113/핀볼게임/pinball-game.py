a8eac6368d8485f3cd4525d1bc792106f613ef57
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2026-01-13 11:46:16 +0900

    Title: 핀볼게임, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 260113/핀볼게임/pinball-game.py
+++ 260113/핀볼게임/pinball-game.py
@@ -5,6 +5,7 @@ n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
+
 def direction_change(d, x, y):
     if grid[x][y] == 1:
         d = d - 1 if d % 2 == 1 else d + 1
@@ -27,29 +28,16 @@ def move_pinball(d, x, y):
             break
 
         d = direction_change(d, x, y)
+    
     return max(ans, move)
 
 dirs = [(1, 0), (0, -1), (-1, 0), (0, 1)]
 ans = 0
 
 for i in range(n):
-    x, y = 0, i
-    d = 0
-    ans = move_pinball(d, x, y)
-
-for i in range(n):
-    x, y = i, n - 1
-    d = 1
-    ans = move_pinball(d, x, y)
-
-for i in range(n):
-    x, y = n - 1, i
-    d = 2
-    ans = move_pinball(d, x, y)
-
-for i in range(n):
-    x, y = i, 0
-    d = 3
-    ans = move_pinball(d, x, y)
+    ans = move_pinball(0, 0, i)
+    ans = move_pinball(1, i, n - 1)
+    ans = move_pinball(2, n - 1, i)
+    ans = move_pinball(3, i, 0)
 
 print(ans)
