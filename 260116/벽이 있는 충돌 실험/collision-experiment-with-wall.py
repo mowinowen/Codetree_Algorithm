8ed8fcc56615315aff91ee7bbda99faaf6d0cee0
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2026-01-16 14:30:46 +0900

    Title: 벽이 있는 충돌 실험, Time: 483ms, Memory: 25MB, Status: Passed - Codetree

--- 260116/벽이 있는 충돌 실험/collision-experiment-with-wall.py
+++ 260116/벽이 있는 충돌 실험/collision-experiment-with-wall.py
@@ -2,62 +2,43 @@ import sys
 input = sys.stdin.readline
 
 T = int(input())
-dirs = {'L' : (0, -1), 'R' : (0, 1), 'U' : (-1, 0), 'D' : (1, 0)}
-reverse_dir = {'L' : 'R', 'R' : 'L', 'U' : 'D', 'D' : 'U'}
+mapper = {'U' : 0, 'D' : 1, 'L' : 2, 'R' : 3}
+dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+reverse_dir = [1, 0, 3, 2]
 
 for _ in range(T):
     n, m = map(int, input().split())
-    grid = [[0] * n for _ in range(n)]
-    dir_grid = [[0] * n for _ in range(n)]
+    marbles = {}
     
     for _ in range(m):
         x, y, d = input().split()
-        x, y = int(x), int(y)
-        grid[x - 1][y - 1] = 1
-        dir_grid[x - 1][y - 1] = d
+        marbles[(int(x) - 1, int(y) - 1)] = mapper[d]
+        
 
     for _ in range(2 * n):
-        next_cnt = [[0] * n for _ in range(n)]
-        next_dir_grid = [[0] * n for _ in range(n)]
-        for x in range(n):
-            for y in range(n):
-                if grid[x][y] == 1:
-                    dx, dy = dirs[dir_grid[x][y]]
-                    nx, ny = x + dx, y + dy
-                    
-                    if 0 <= nx < n and 0 <= ny < n:
-                        next_cnt[nx][ny] += 1
-                        next_dir_grid[nx][ny] = dir_grid[x][y]
-                    
-                    else:
-                        next_cnt[x][y] += 1
-                        next_dir_grid[x][y] = reverse_dir[dir_grid[x][y]]
+        next_marbles = {}
+        bomb = set()
+
+        for (x, y), d in marbles.items():
+            dx, dy = dirs[d]
+            nx, ny = x + dx, y + dy
+            nd = d
+            
+            if not(0 <= nx < n and 0 <= ny < n):
+                nd = reverse_dir[d]
+                nx, ny = x, y
+
+            if (nx, ny) in next_marbles:
+                bomb.add((nx, ny))
+            else:
+                next_marbles[(nx, ny)] = nd
         
-        # for row in next_cnt:
-        #     print(*row)
-        # print()
-        # for row in next_dir_grid:
-        #     print(*row)
-        # print()
+        for x, y in bomb:
+            del next_marbles[(x, y)]
+        
+        marbles = next_marbles
 
-        for x in range(n):
-            for y in range(n):
-                if next_cnt[x][y] > 1:
-                    next_cnt[x][y] = 0
-                    next_dir_grid[x][y] = 0
+        if not marbles:
+            break
 
-        grid = next_cnt
-        dir_grid = next_dir_grid
-
-        # for row in grid:
-        #     print(*row)
-        # print()
-        # for row in dir_grid:
-        #     print(*row)
-        # print()
-
-    ans = 0
-    for row in grid:
-        ans += row.count(0)
-    
-    print(n ** 2 - sum(row.count(0) for row in grid))
+    print(len(marbles))
