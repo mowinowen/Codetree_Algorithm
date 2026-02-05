7a14547b99876cdf2db10e723d89d10d4e0e03d7
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2026-02-05 15:15:17 +0900

    Title: 사다리 타기, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 260205/사다리 타기/ladder-game.py
+++ 260205/사다리 타기/ladder-game.py
@@ -1,37 +1,13 @@
 import sys
-from itertools import combinations
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 # Please write your code here.
 
-bridges = []
-for _ in range(m):
-    x, y = map(int, input().split())
-    bridges.append((x, y))
-
-bridges.sort(key = lambda x : x[1])
-
+bridges = sorted([tuple(map(int, input().split())) for _ in range(m)], key = lambda x : x[1])
 players = [i for i in range(n + 1)]
 
 for x, y in bridges:
     players[x], players[x + 1] = players[x + 1], players[x]
 
-ans_players = players
-isend = False
-for i in range(m + 1):
-    for new_bridges in combinations(bridges, i):
-        players = [i for i in range(n + 1)]
-
-        for x, y in new_bridges:
-            players[x], players[x + 1] = players[x + 1], players[x]
-        
-        if players == ans_players:
-            ans = i
-            isend = True
-            break
-    
-    if isend:
-        break
-
-print(ans)
+print(sum(players[i] > players[j] for i in range(1, n + 1) for j in range(i + 1, n + 1)))
