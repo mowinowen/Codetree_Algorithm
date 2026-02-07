989407013a333016484eb7205cf924cef3adae4b
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2026-02-07 20:18:26 +0900

    Title: 특정 조건에 맞게 K개 중에 1개를 N번 뽑기, Time: 50ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260207/특정 조건에 맞게 K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
@@ -0,0 +1,14 @@
+import sys
+from itertools import product
+
+input = sys.stdin.readline
+K, N = map(int, input().split())
+
+# Please write your code here.
+for nums in product(range(1, K + 1), repeat = N):
+    if len(nums) > 2:
+        for i in range(N - 2):
+            if not (nums[i] == nums[i + 1] == nums[i + 2]):
+                print(*nums)
+    else:
+        print(*nums)
