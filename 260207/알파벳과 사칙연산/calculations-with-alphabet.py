ae06d2fe212c8ffb72372ecc70d5f574842b8cd5
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2026-02-07 15:32:06 +0900

    Title: 알파벳과 사칙연산, Time: 61ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 260207/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -0,0 +1,23 @@
+import sys
+from itertools import product
+input = sys.stdin.readline
+
+expression = input().strip()
+
+# Please write your code here.
+operator = ['+'] + [e for e in expression if e in ['+', '-', '*']]
+
+ans = 0
+for nums in product(range(1, 5), repeat = len(operator) + 1):
+    sum_num = 0
+    for i in range(len(operator)):
+        if operator[i] == '+':
+            sum_num += nums[i]
+        elif operator[i] == '-':
+            sum_num -= nums[i]
+        else:
+            sum_num *= nums[i]
+        
+    ans = max(ans, sum_num)
+
+print(ans)
