07e6c0c70ef60422379b95097e8b8a26fc37eeea
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2026-02-07 16:44:30 +0900

    Title: 알파벳과 사칙연산, Time: 70ms, Memory: 17MB, Status: Passed - Codetree

--- 260207/알파벳과 사칙연산/calculations-with-alphabet.py
+++ 260207/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -6,18 +6,28 @@ expression = input().strip()
 
 # Please write your code here.
 operator = ['+'] + [e for e in expression if e in ['+', '-', '*']]
+num_and_operator = [('+', expression[0])]
+num_dict = {expression[0] : 0}
+
+for i in range(1, len(expression) - 1, 2):
+    num_and_operator.append((expression[i], expression[i + 1]))
+    if not expression[i + 1] in num_dict:
+        num_dict[expression[i + 1]] = 0
 
 ans = float('-inf')
-for nums in product(range(1, 5), repeat = len(operator)):
+for nums in product(range(1, 5), repeat = len(num_dict)):
+    for num, c in zip(nums,num_dict):
+        num_dict[c] = num
+
     sum_num = 0
-    for i in range(len(operator)):
-        if operator[i] == '+':
-            sum_num += nums[i]
-        elif operator[i] == '-':
-            sum_num -= nums[i]
+    for op, c in num_and_operator:
+        if op == '+':
+            sum_num += num_dict[c]
+        elif op == '-':
+            sum_num -= num_dict[c]
         else:
-            sum_num *= nums[i]
-    
+            sum_num *= num_dict[c]
+
     ans = max(ans, sum_num)
 
 print(ans)
