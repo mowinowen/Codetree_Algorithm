f1a7633d409eb86d17a2fff4000fc9f0b1919429
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2026-02-18 22:00:31 +0900

    Title: 외판원 순회, Time: 175ms, Memory: 22MB, Status: Passed - Codetree

--- 260218/외판원 순회/traveling-salesman-problem.py
+++ 260218/외판원 순회/traveling-salesman-problem.py
@@ -5,25 +5,23 @@ n = int(input())
 A = [list(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
-result = []
 visited = [0] * n
 ans = float('inf')
 
 def dfs(cnt, curr, start):
     global ans
+    if curr >= ans:
+        return
     if cnt == n - 1:
-        if not A[start][0]:
-            return
-        ans = min(ans, curr + A[start][0])
+        if A[start][0]:
+            ans = min(ans, curr + A[start][0])
         return
     
     for i in range(2, n + 1):
-        if not A[start][i - 1]:
-            continue
-        if not visited[i - 1]:
+        if not visited[i - 1] and A[start][i - 1]:
             visited[i - 1] = 1
             dfs(cnt + 1, curr + A[start][i - 1], i - 1)
-            visited[i - 1] = 0
+            visited[i - 1] = 0            
 
 dfs(0, 0, 0)
 print(ans)
