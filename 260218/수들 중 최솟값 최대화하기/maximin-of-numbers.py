6d8b6b1524062e8c2d2edd9858aaa8355d346de6
Owen Kim (Dong Hyun) <88826811+mowinowen@users.noreply.github.com>
2026-02-18 22:35:08 +0900

    Title: 수들 중 최솟값 최대화하기, Time: 199ms, Memory: 23MB, Status: Passed - Codetree

---
+++ 260218/수들 중 최솟값 최대화하기/maximin-of-numbers.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+visited = [0] * n
+ans = 0
+
+def dfs(cnt, curr):
+    global ans
+    if cnt == n:
+        ans = max(ans, curr)
+        return
+    
+    for i in range(n):
+        if not visited[i]:
+            visited[i] = 1
+            dfs(cnt + 1, min(curr, grid[cnt][i]))
+            visited[i] = 0
+
+dfs(0, float('inf'))
+print(ans)
